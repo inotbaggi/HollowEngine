b5c0acb91e333a74bf61b2d7ce287f86a379e162
_BENDY659_ <denisglazyrin13@gmail.com>
2025-02-12 16:56:54 +0900

    Начало приключений. Удалён мусор (не в обиду)

---
+++ docs/src/commonMain/kotlin/ru/hollowhorizon/hollowengine/docs/pages/SimpleTextPage.kt
@@ -0,0 +1,7 @@
+package ru.hollowhorizon.hollowengine.docs.pages
+
+import de.fabmax.kool.modules.ui2.Text
+import de.fabmax.kool.modules.ui2.UiScope
+import de.fabmax.kool.modules.ui2.textColor
+import de.fabmax.kool.util.Color
+
--- src/main/java/ru/hollowhorizon/hollowengine/client/gui/scripting/panels/DocsTreePanel.kt
+++ src/main/java/ru/hollowhorizon/hollowengine/client/gui/scripting/panels/DocsTreePanel.kt
@@ -2,41 +2,22 @@ package ru.hollowhorizon.hollowengine.client.gui.scripting.panels
 
 import de.fabmax.kool.modules.ui2.UiScope
 import de.fabmax.kool.modules.ui2.docking.Dock
-import ru.hollowhorizon.hollowengine.HollowEngine
 import ru.hollowhorizon.hollowengine.client.gui.docs.DocsNode
 import ru.hollowhorizon.hollowengine.client.gui.scripting.FileNode
-import ru.hollowhorizon.hollowengine.docs.pages.CreditsPage
 import ru.hollowhorizon.hollowengine.docs.pages.WelcomePage
-import ru.hollowhorizon.hollowengine.docs.pages.begin.DirectoryPage
-import ru.hollowhorizon.hollowengine.docs.pages.testing.TestFontPage
-import ru.hollowhorizon.hollowengine.docs.pages.testing.TestImages
 
 class DocsTreePanel(dock: Dock) : DockPanel("hollowengine.gui.ide.docs", dock) {
-    override val icon = "hollowengine:textures/gui/icons/docs.svg"
+    override val icon = "hollowengine:textures/gui/icons/docs.png"
 
     override fun UiScope.compose() {
         docsTree()
     }
 
     val docsTree = DocsNode("HollowEngine", "").apply {
-        //initPages()
+        initPages()
 
         isFolder = true
-
         children += DocsNode("welcome", WelcomePage)
-        children += DocsNode("credits", CreditsPage)
-        children += DocsNode("begin").apply {
-            isFolder = true
-
-            children += DocsNode("begin.directory", DirectoryPage)
-        }
-
-        if(HollowEngine.config.docsDebug.debugPages)
-            children += DocsNode("Testing Page", "").apply {
-                isFolder = true
-                children += DocsNode("FONT", TestFontPage)
-                children += DocsNode("IMAGES", TestImages)
-            }
     }.resize()
 
     private fun initPages() {}
