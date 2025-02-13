46352c615ee3260204f36346a54d98f746734d53
_BENDY659_ <denisglazyrin13@gmail.com>
2025-02-13 11:41:55 +0900

    xd

--- src/main/java/ru/hollowhorizon/hollowengine/client/gui/scripting/panels/DocsTreePanel.kt
+++ src/main/java/ru/hollowhorizon/hollowengine/client/gui/scripting/panels/DocsTreePanel.kt
@@ -4,6 +4,7 @@ import de.fabmax.kool.modules.ui2.UiScope
 import de.fabmax.kool.modules.ui2.docking.Dock
 import ru.hollowhorizon.hollowengine.client.gui.docs.DocsNode
 import ru.hollowhorizon.hollowengine.client.gui.scripting.FileNode
+import ru.hollowhorizon.hollowengine.docs.pages.TestPage
 import ru.hollowhorizon.hollowengine.docs.pages.WelcomePage
 
 class DocsTreePanel(dock: Dock) : DockPanel("hollowengine.gui.ide.docs", dock) {
@@ -16,6 +17,8 @@ class DocsTreePanel(dock: Dock) : DockPanel("hollowengine.gui.ide.docs", dock) {
     val docsTree = DocsNode("HollowEngine", "").apply {
         isFolder = true
         children += DocsNode("welcome", WelcomePage)
+
+        children += DocsNode("test", TestPage)
     }.resize()
 
     private fun FileNode.resize(depth: Int = 0): FileNode {
