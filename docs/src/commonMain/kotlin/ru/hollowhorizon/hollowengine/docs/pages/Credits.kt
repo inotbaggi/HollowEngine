a391be661cfe897243a3fa6c2abb283ff28465c9
_BENDY659_ <denisglazyrin13@gmail.com>
2025-02-16 16:18:30 +0900

    кхм

--- docs/src/commonMain/kotlin/ru/hollowhorizon/hollowengine/docs/pages/Credits.kt
+++ docs/src/commonMain/kotlin/ru/hollowhorizon/hollowengine/docs/pages/Credits.kt
@@ -6,21 +6,32 @@ import de.fabmax.kool.math.Vec2f
 import de.fabmax.kool.modules.ui2.*
 import de.fabmax.kool.pipeline.Texture2d
 import de.fabmax.kool.util.Color
-import de.fabmax.kool.util.MsdfFont
-import ru.hollowhorizon.hollowengine.docs.HACK_FONT
 import ru.hollowhorizon.hollowengine.docs.Header
 import ru.hollowhorizon.hollowengine.docs.divide
 import ru.hollowhorizon.hollowengine.docs.shaders.BlurImageShader
 import ru.hollowhorizon.hollowengine.docs.text
 
 object Credits: Composable {
-    enum class Author(val nick: String, val color: String, val avatarId: String) {
-        NONE("NONE_AUTHOR_SELECTED", "777777", "none"),
-        BENDY659("_BENDY659_", "ad0e0e", "bendy659"),
-        THEHOLLOWHORIZON("HollowHorizon", "db911a", "thehollowhorizon"),
-        UNKNOWN("Неизвестный", "551270", "unknown")
+    enum class Author(val nick: String, val color: String) {
+        NONE("NONE_AUTHOR_SELECTED", "777777"),
+        BENDY659("_BENDY659_", "ad0e0e"),
+        THEHOLLOWHORIZON("HollowHorizon", "db911a"),
+        UNKNOWN("Неизвестный", "551270")
     }
-    var authorSelect = Author.NONE
+    private var authorSelect = Author.NONE
+
+    private val authors = listOf(
+        Author.NONE,
+        Author.THEHOLLOWHORIZON,
+        Author.BENDY659,
+        Author.UNKNOWN
+    )
+    private val avatars = listOf(
+        Texture2d { Assets.loadImage2d("hollowengine:docs/authors/none.png").getOrThrow() },
+        Texture2d { Assets.loadImage2d("hollowengine:docs/authors/thehollowhorizon.png").getOrThrow() },
+        Texture2d { Assets.loadImage2d("hollowengine:docs/authors/bendy659.png").getOrThrow() },
+        Texture2d { Assets.loadImage2d("hollowengine:docs/authors/unknown.png").getOrThrow() }
+    )
 
     override fun UiScope.compose() {
         text("Над документацией работали", Header.H1)
@@ -29,62 +40,50 @@ object Credits: Composable {
 
         divide()
 
-        Box { modifier.align(AlignmentX.Center, AlignmentY.Top)
-            Row {
-                authorButton(Author.NONE.nick, Author.NONE.color) { authorSelect = Author.NONE }
-                authorButton(Author.THEHOLLOWHORIZON.nick, Author.THEHOLLOWHORIZON.color) { authorSelect = Author.THEHOLLOWHORIZON }
-                authorButton(Author.BENDY659.nick, Author.BENDY659.color) { authorSelect = Author.BENDY659 }
-                authorButton(Author.UNKNOWN.nick, Author.UNKNOWN.color) { authorSelect = Author.UNKNOWN }
-            }
+        var selectIndex by remember { mutableStateOf(0) }
+
+        ComboBox {
+            modifier
+                .align(AlignmentX.Center, AlignmentY.Center)
+                .margin(sizes.gap)
+                .items(authors.map { it.nick })
+                .selectedIndex(selectIndex)
+                .backgroundColor(Color(authorSelect.color))
+                .onItemSelected {
+                    authorSelect = authors[it]
+                    selectIndex = it
+                    surface.triggerUpdate()
+                }
+                .size(Grow.Std, FitContent)
+
         }
 
         divide(Color(authorSelect.color))
 
         // ==== //
 
+        avatar(avatars[selectIndex])
+        divide(Color(authorSelect.color))
+
         when(authorSelect) {
             Author.NONE -> {
-                avatar(authorSelect.avatarId)
-                divide(Color(authorSelect.color))
                 text("Выбери любого для дополнительной информации")
             }
             Author.THEHOLLOWHORIZON -> {
-                avatar(authorSelect.avatarId)
-                divide(Color(authorSelect.color))
                 text("Является техническим разработчиком документации.")
                 text("Холоу там потом сам себе добавить если надо :D")
             }
             Author.BENDY659 -> {
-                avatar(authorSelect.avatarId)
-                divide(Color(authorSelect.color))
                 text("Основной документатор документации.")
                 text("Самый ебнарь года, но титул конченного достаётся всё же - TheHollowHorizon :D")
             }
             Author.UNKNOWN -> {
-                avatar(authorSelect.avatarId)
-                divide(Color(authorSelect.color))
                 text("Может это будешь ты?")
                 text("Тебе всего-то нужно принести в доки многа контента (не всё подряд, что потребуют лентяи), а только реально важное :D")
             }
         }
     }
-
-    private fun UiScope.authorButton(text: String, color: String, onClick: () -> Unit) = Button(text) {
-        modifier
-            .font(MsdfFont(HACK_FONT))
-            .colors(buttonColor = Color(color))
-            .textColor(Color.WHITE)
-            .border(RoundRectBorder(Color.BLACK, 2.dp, sizes.borderWidth + 1.dp))
-            .onHover {
-                modifier
-                    .textColor(Color.BLACK)
-                    .border(RoundRectBorder(Color.WHITE, 2.dp, sizes.borderWidth + 2.dp))
-            }
-            .onClick { onClick() }
-    }
-    fun UiScope.avatar(id: String) = Image(remember {
-        Texture2d { Assets.loadImage2d("hollowengine:docs/authors/$id.png").getOrThrow() }
-    }) {
+    private fun UiScope.avatar(id: Texture2d) = Image(id) {
         val shader = BlurImageShader()
 
         modifier
