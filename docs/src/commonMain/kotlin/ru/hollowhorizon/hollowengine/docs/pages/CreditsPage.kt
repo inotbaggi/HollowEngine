00c52bc2d8fc2624a83afba955316ec339e79274
_BENDY659_ <denisglazyrin13@gmail.com>
2025-04-27 20:14:19 +0900

    Кажется всё

---
+++ docs/src/commonMain/kotlin/ru/hollowhorizon/hollowengine/docs/pages/CreditsPage.kt
@@ -0,0 +1,97 @@
+package ru.hollowhorizon.hollowengine.docs.pages
+
+import de.fabmax.kool.math.Vec2f
+import de.fabmax.kool.modules.ui2.*
+import de.fabmax.kool.pipeline.Texture2d
+import de.fabmax.kool.util.Color
+import ru.hollowhorizon.hollowengine.docs.Header
+import ru.hollowhorizon.hollowengine.docs.divide
+import ru.hollowhorizon.hollowengine.docs.loadImage
+import ru.hollowhorizon.hollowengine.docs.shaders.BlurImageShader
+import ru.hollowhorizon.hollowengine.docs.text
+
+object CreditsPage: Composable {
+    enum class Author(val nick: String, val color: String) {
+        NONE("NONE_AUTHOR_SELECTED", "777777"),
+        BENDY659("_BENDY659_", "ad0e0e"),
+        THEHOLLOWHORIZON("HollowHorizon", "db911a"),
+        UNKNOWN("Неизвестный", "551270")
+    }
+    private var authorSelect = Author.NONE
+    private val authors = listOf(Author.NONE, Author.THEHOLLOWHORIZON, Author.BENDY659, Author.UNKNOWN)
+
+        override fun UiScope.compose() {
+            val avatars = listOf(
+                loadImage("authors/none.png"),
+                loadImage("authors/thehollowhorizon.png"),
+                loadImage("authors/bendy659.png"),
+                loadImage("authors/unknown.png")
+            )
+
+            text("Над документацией работали", Header.H1)
+
+            // ==== //
+
+            divide()
+
+            var selectIndex by remember { mutableStateOf(0) }
+
+            ComboBox {
+                modifier
+                    .align(AlignmentX.Center, AlignmentY.Center)
+                    .margin(sizes.gap)
+                    .items(authors.map { it.nick })
+                    .selectedIndex(selectIndex)
+                    .backgroundColor(Color(authorSelect.color))
+                    .onItemSelected {
+                        selectIndex = it
+                        authorSelect = authors[it]
+                    }
+                    .size(Grow.Std, FitContent)
+
+            }
+
+            divide(Color(authorSelect.color))
+
+            // ==== //
+
+            avatar(avatars[selectIndex])
+            divide(Color(authorSelect.color))
+
+            when(authorSelect) {
+                Author.NONE -> {
+                    text("Выбери любого для дополнительной информации")
+                }
+                Author.THEHOLLOWHORIZON -> {
+                    text("Является техническим разработчиком документации.")
+                    text("Холоу там потом сам себе добавить если надо :D")
+                }
+                Author.BENDY659 -> {
+                    text("Основной документатор документации.")
+                    text("Самый ебнарь года, но титул конченного достаётся всё же - TheHollowHorizon :D")
+                }
+                Author.UNKNOWN -> {
+                    text("Может это будешь ты?")
+                    text("Тебе всего-то нужно принести в доки многа контента (не всё подряд, что потребуют лентяи), а только реально важное :D")
+                    text("А может я шучу?)")
+                    text("А может - пошёл я?!")
+                }
+            }
+        }
+        private fun UiScope.avatar(id: Texture2d) = Image(id) {
+            val shader = BlurImageShader()
+
+            modifier
+                .imageSize(ImageSize.FitContent)
+                .alignX(AlignmentX.Center)
+                .size(Grow(0.9f, Grow.Std), FitContent)
+                .customShader(shader)
+                .onPositioned {
+                    modifier.imageProvider?.getTexture(uiNode.innerWidthPx, uiNode.innerHeightPx)?.let {
+                        shader.image = it
+                        shader.resolution = Vec2f(uiNode.innerWidthPx, uiNode.innerHeightPx)
+                        shader.power = 0.1f
+                    }
+                }
+        }
+}
---
+++ docs/src/commonMain/kotlin/ru/hollowhorizon/hollowengine/docs/pages/WelcomePage.kt
@@ -0,0 +1,67 @@
+package ru.hollowhorizon.hollowengine.docs.pages
+
+import de.fabmax.kool.modules.ui2.*
+import de.fabmax.kool.util.Color
+import ru.hollowhorizon.hollowengine.docs.*
+import kotlin.random.Random
+
+/*
+ * Спасибо тем, кто голосовал за доки в опросе, я рад за вас.
+ * Остальным - соболезную. Хотя как, если вы и так знаете как работать с HollowEngine, потому что умеете читать исходники - то я рад за вас,
+ * но вот если вы вообще не понимаете и проголосовали за всё остальное с надеждой, что вам разжуют — вам соболезную. Вы опустились для меня.
+*/
+
+object WelcomePage : Composable {
+    private var r = Random.nextInt(0, 101)
+    private val titleImg: () -> Pair<String, Float> = {
+        if(r in 96..98)
+            "welcome" to 0.1f
+        else if(r in 98..100)
+            "welcome2_s" to 0.05f
+        else
+            "welcome2" to 0.05f
+    }
+
+    private val sHide0 = mutableStateOf(false)
+
+    override fun UiScope.compose() {
+        text("Добро пожаловать", Header.H1)
+        text("на официальную документацию по моду", Header.H2)
+        text("\"HollowEngine\"", Header.H3, bold = true)
+        br()
+        title(titleImg().first, titleImg().second)
+        divbr()
+        text("Данная документация должна обучить вас основам скриптинга в данном моде.")
+        br()
+        spoiler("Спойлер", sHide0) {
+            text("Не нада со слезами бежать в канал #помощь и там просить-молить чтоб вам помогли. >:(")
+            text("Изучите документацию обязательно !!!ПОЛНОСТЬЮ!!! Потому что скорее всего ваша проблема не то что решена, а абсолютно абсурдна!")
+            text("Это значит что вы задаёте вопрос, когда ответ есть прямо в документации, но вы почему-то не хотите её читать полностью :(")
+            text("Я не знаю как вы там доки блин читаете. Не уж то реально, до 1-го предложения и потом сидите и - \"Пипец я умный\"!", Header.H6, italic = true)
+        }
+        divbr()
+        table("Язык программирования Kotlin", TableType.TIP) {
+            text("Перед прочтением документации, рекомендуется ознакомится с таким языком программирования как \"Kotlin\"!")
+            text("Без знания хотя бы \"Базового уровня\" - вам будет сложно понимать систему работы скриптов.")
+            // ==== //
+            Box { modifier.align(AlignmentX.Center, AlignmentY.Bottom)
+                button("Документация по Kotlin", type = ButtonType.LINK) { openUrl("https://kotlinlang.org/docs/basic-syntax.html") }
+            }
+        }
+        table("Внимание", TableType.WARN) {
+            text("Это бета-версия документации, так что всё в будущем может изменится.")
+            divide(Color(TableType.WARN.border))
+            text("При обнаружении ошибок сообщите или на GitHub Issues или на Discord сервере Phase Of Horizon.")
+            text("Успехов в разработке!")
+
+            // ==== //
+
+            Box { modifier.align(AlignmentX.Center, AlignmentY.Bottom)
+                Row {
+                    button("GitHub Issues", type = ButtonType.LINK) { openUrl("https://github.com/HollowHorizon/HollowEngine/issues") }
+                    button("Discord", type = ButtonType.LINK) { openUrl("https://discord.gg/qYzFXXpzZk") }
+                }
+            }
+        }
+    }
+}
